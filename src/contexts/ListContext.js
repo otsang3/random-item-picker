36bcb1988ad31a75b965bb7e6f9db5363bfe8126
otsang3 <ohhtsang@gmail.com>
2020-09-04 15:04:44 +0100

    added addListItem, deleteListItem functions in ListContext.js

--- src/contexts/ListContext.js
+++ src/contexts/ListContext.js
@@ -1,8 +1,23 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const ListContext = createContext();
 
 function ListContextProvider(props) {
+
+    const [list, setList] = useState([])
+
+    const addListItem = (listItem) => {
+        setState(prevState => [
+            ...prevState, listItem
+        ])
+    }
+
+    const deleteListItem = (index) => {
+        setState(prevState => {
+            prevState.splice(index, 1);
+            return [...prevState]
+        })
+    }
 
     return(
         <ListContext.Provider>
